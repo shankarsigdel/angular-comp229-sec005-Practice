43b0656045fba385c78f9f7498bce53242f6b51a
shankarsigdel <shankarsigdel26@gmail.com>
2020-11-30 17:47:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-comp229-sec005-rqjaw7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-comp229-sec005-rqjaw7)
---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: "1",
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens",
+    imageUrl:
+      "https://i.insider.com/5e625eccfee23d0c3b666de8?width=1136&format=jpeg"
+  },
+  {
+    id: "2",
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras",
+    imageUrl:
+      "https://cdn.pocket-lint.com/r/s/1200x630/assets/images/149324-phones-review-review-iphone-11-pro-max-review-product-shots-image1-keant0hfcg.jpg"
+  },
+  {
+    id: "3",
+    name: "Phone Standard",
+    price: 299,
+    description: "",
+    imageUrl:
+      "https://cdn.pocket-lint.com/r/s/1200x630/assets/images/149324-phones-review-review-iphone-11-pro-max-review-product-shots-image1-keant0hfcg.jpg"
+  },
+  {
+    id: "4",
+    name: "Android Phone",
+    price: 199,
+    description: "",
+    imageUrl:
+      "https://i.insider.com/5e625eccfee23d0c3b666de8?width=1136&format=jpeg"
+  }
+];
